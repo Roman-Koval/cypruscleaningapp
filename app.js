8b111397b00a78dee7ed5cae47a8aceb457e2f07
Roman-Koval <kovalrv83@gmail.com>
2026-06-01 22:31:54 +0100

    Initial project upload

---
+++ app.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
+import { getMessaging } from "firebase/messaging";
+
+// Firebase config (замени на свои данные из Firebase Console)
+const firebaseConfig = {
+  apiKey: "YOUR_API_KEY",
+  authDomain: "YOUR_PROJECT.firebaseapp.com",
+  projectId: "YOUR_PROJECT",
+  storageBucket: "YOUR_PROJECT.appspot.com",
+  messagingSenderId: "YOUR_SENDER_ID",
+  appId: "YOUR_APP_ID"
+};
+
+const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
+export const auth = getAuth(app);
+export const messaging = getMessaging(app);
+
+console.log("SupTemiz PWA запущен!");
