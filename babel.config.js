bf7e4bc57bf41d39ae4dd1f39d2fd9e4567b97b4
Roman Koval <77807168+Roman-Koval@users.noreply.github.com>
2026-06-01 17:18:37 +0100

    Create babel.config.js

---
+++ babel.config.js
@@ -0,0 +1,15 @@
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: [
+      [
+        'babel-preset-expo',
+        {
+          // Transform import.meta for web compatibility
+          unstable_transformImportMeta: true,
+        },
+      ],
+    ],
+    plugins: ['react-native-worklets/plugin'],
+  };
+};
